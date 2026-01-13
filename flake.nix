baeeca972a4083767cf42f3084d1e3dce5d8e9b9
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-12 23:54:12 -0300

    Add jekyll (through a nix flake) to serve the site locally like github would

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Website for Papier, a strict, small document language";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              just
+
+              jekyll
+            ];
+          };
+        }
+      );
+    };
+}
